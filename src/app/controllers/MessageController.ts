44272a52674dadc2345be4b20f82f38c33f35526
AmauriLima <amauri.plimaj@gmail.com>
2021-10-20 16:48:32 -0300

    feat: implementing socket.io

--- src/app/controllers/MessageController.ts
+++ src/app/controllers/MessageController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import socketIo from 'socket.io';
 import PrismaClient from '../prisma';
 
 class MessageController {
@@ -6,6 +7,8 @@ class MessageController {
     const { message: text } = request.body;
 
     const { user_id } = request;
+
+    const io = request.io as socketIo.Server;
 
     const message = await PrismaClient.message.create({
       data: {
@@ -16,6 +19,18 @@ class MessageController {
         user: true,
       },
     });
+
+    const infoWS = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.created_at,
+      user: {
+        name: message.user.name,
+        avatar_url: message.user.avatar_url,
+      },
+    };
+
+    io.emit('new_message', infoWS);
 
     response.status(201).json(message);
   }
