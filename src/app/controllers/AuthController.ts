3de19395b6caef6e436959974d1e4369ba0be8b5
AmauriLima <amauri.plimaj@gmail.com>
2021-10-20 13:12:44 -0300

    feat: connection with DB and adding user register

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
 import { Request, Response } from 'express';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+
+import PrismaClient from '../prisma';
 
 interface IAccessTokenResponse {
   access_token: string,
@@ -24,6 +27,7 @@ class AuthController {
 
   async authenticate(request: Request, response: Response) {
     const { code } = request.body;
+
     const url = 'https://github.com/login/oauth/access_token';
 
     const { data: { access_token } } = await axios.post<IAccessTokenResponse>(url, null, {
@@ -37,13 +41,55 @@ class AuthController {
       },
     });
 
-    const { data: userData } = await axios.get<IUserResponse>('https://api.github.com/user', {
-      headers: {
-        authorization: `Bearer ${access_token}`,
+    let userData;
+    try {
+      const { data } = await axios.get<IUserResponse>('https://api.github.com/user', {
+        headers: {
+          authorization: `Bearer ${access_token}`,
+        },
+      });
+      userData = data;
+    } catch (err) {
+      return response.status(401).json({ err: err.message });
+    }
+
+    const {
+      id, login, name, avatar_url,
+    } = userData;
+
+    let user = await PrismaClient.user.findFirst({
+      where: {
+        github_id: id,
       },
+
     });
 
-    response.json(userData);
+    if (!user) {
+      user = await PrismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = jwt.sign({
+      user: {
+        name: user.name,
+        avatar_url: user.avatar_url,
+        id: user.id,
+      },
+    }, process.env.JWT_SECRET,
+    {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    response.json({
+      user, token,
+    });
   }
 }
 
